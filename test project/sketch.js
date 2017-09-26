d6648c01b1f415f17bfefde8fc6a81dad0001649
hawk-3y3 <hawk-3y3@users.noreply.github.com>
2017-09-26 22:22:10 +0200

    initial commit

---
+++ test project/sketch.js
@@ -0,0 +1,40 @@
+let x = 0
+let y = 0
+let spacing = 25
+
+function setup() {
+    createCanvas(400, 400);
+    background(0);
+    stroke(255)
+}
+
+function draw(){
+    if (random(1) < 0.5){
+        line(x,y,x + spacing,y + spacing)
+    } else {
+        line(x + spacing, y, x, y + spacing)
+    }
+    
+    x = x + spacing
+
+    if(x > width){
+        x = 0
+        y= y + spacing
+    }
+
+    if(y > height){
+        y = 0
+        stroke(colorPick())
+    }
+}
+
+function colorPick(){
+    let input = random(3)
+    if (input < 1){
+        return "red"
+    } else if( input < 2){
+        return "blue" 
+    } else if(input < 3){
+        return "green"
+    }
+}
