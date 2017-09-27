738849f04f8ee34acc8c8db8a7e553aff291c934
hawk-3y3 <hawk-3y3@users.noreply.github.com>
2017-09-27 03:39:44 +0200

    now going both ways

--- 10 print loop/sketch.js
+++ 10 print loop/sketch.js
@@ -1,40 +1,103 @@
 let x = 0
-let y = 0
-let spacing = window.innerWidth/20
+let y = window.innerHeight
+let scale = 75
+let direction
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(window.innerWidth - 5, window.innerHeight - 5);
     background(0);
     stroke(255)
+    pickColor();
+    pickDirection();
 }
 
 function draw(){
-    if (random(1) < 0.5){
-        line(x,y,x + spacing,y + spacing)
+    if (direction == "up"){
+        moveUp()
+    } else if ( direction == "down"){
+        moveDown()
+    } else if (direction == "left"){
+        moveLeft()
     } else {
-        line(x + spacing, y, x, y + spacing)
-    }
-    
-    x = x + spacing
-
-    if(x > width){
-        x = 0
-        y= y + spacing
-    }
-
-    if(y > height){
-        y = 0
-        stroke(colorPick())
+        moveRight()
     }
 }
 
-function colorPick(){
-    let input = random(3)
-    if (input < 1){
-        return "red"
-    } else if( input < 2){
-        return "blue" 
-    } else if(input < 3){
-        return "green"
+function pickColor(){
+    let colors = ["red", "green", "blue", "magenta", "yellow", "cyan"]
+    return random(colors)
+}
+
+
+function moveDown() {
+    drawLine()
+    x += scale
+    if (x >= window.innerWidth){
+        y += scale
+        x = 0
+        if (y >= window.innerHeight){
+            stroke(pickColor())
+            pickDirection()
+        }
+    }
+}
+
+function moveUp(){
+    drawLine()
+    x -= scale
+    if (x <= 0){
+        y -= scale
+        x = window.innerWidth
+        if (y <= -scale){
+            stroke(pickColor())
+            pickDirection()
+        }
+    }
+}
+
+function moveLeft(){
+    drawLine()
+    y -= scale
+    if (y <= 0){
+        x -= scale
+        y = window.innerHeight
+        if (x <= - scale){
+            stroke(pickColor())
+            pickDirection()
+        }
+    }
+}
+
+function moveRight(){
+    drawLine()
+    y += scale
+    if (y >= window.innerHeight){
+        x += scale
+        y = 0
+        if (x >= window.innerWidth){
+            stroke(pickColor())
+            pickDirection()
+        }
+    }
+}
+
+function drawLine() {
+    if(random(1)<0.5) {
+        line(x, y, x + scale, y + scale);
+    } else {
+        line(x + scale, y, x, y + scale);
+    }
+}
+
+function pickDirection(){
+    let directions = ["up", "down", "left", "right"]
+    direction =  random(directions)
+    console.log(direction)
+    if (direction == "up"||direction == "left"){
+        y = Math.ceil((scale/window.innerHeight))*window.innerHeight
+        x = Math.ceil((scale/window.innerWidth))*window.innerWidth
+    } else {
+        y = 0
+        x = 0
     }
 }
